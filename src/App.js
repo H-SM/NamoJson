ec5a26d4456643837cc22d1577d0d2d41c455964
H-SM <harmanmalht777@gmail.com>
2024-05-10 22:40:26 +0530

    feat (build): Initialized Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import logo from "./assets/namoJson_logo.png"
+function App() {
+  return (
+    <div className="bg-background">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'text': '#060507',
+      'background': '#fafafc',
+      'primary': '#6963a2',
+      'secondary': '#a7a3cf',
+      'accent': '#837cc3',
+     },        
+  },
+  plugins: [],
+}
+
